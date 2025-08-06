7399728035a76b8edad77e4d4954a1fce40ddb42
Kunal Bansal <kunal@Kunals-MacBook-Pro-2.local>
2025-08-06 17:25:45 +0530

    Initial commit

---
+++ pages/platform.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold">Platfrom</h1>
+      <p className="mt-4">This page will contain information about AgriKunba's mission, team, and story.</p>
+    </main>
+  );
+}
