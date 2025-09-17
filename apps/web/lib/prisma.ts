707f527b22e89d7739ae884c14a13caf471c1da9
Kunal Bansal <kunal@Kunals-MacBook-Pro-2.local>
2025-09-17 19:17:46 +0530

    DB addition2

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error"], // keep logs light
+   log: ["query", "error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
